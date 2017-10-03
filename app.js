a51a0ac272bff0e9a0f33c7fd0f41634ed213b3f
Foster Hersey <foster@users.noreply.github.com>
2017-10-03 03:14:23 -0400

    Add POST

--- app.js
+++ app.js
@@ -31,24 +31,27 @@ let control;
 // HTTP1 support will be handled by the unknownProtocol event handler
 const server = createSecureServer({ key, cert, 'allowHTTP1': false }, (req, res) => {
     if ( control ) {
-        const forwardReq = control.request({ ':method': req.method, ':path': req.url, ':authority': req.headers.host, ':scheme': 'https' });
+        const forwardReq = control.request({
+            ':method': req.method,
+            ':path': req.url,
+            ':authority': req.headers.host,
+            ':scheme': 'https',
+            'content-type': req.headers['content-type']
+        });
         forwardReq.on('response', (headers) => {
-            debugger;
-            res.writeHead(headers[':status']);
+            const forwardHeaders = {};
+            if ( Object.prototype.hasOwnProperty.call(headers, 'content-type') ) {
+                forwardHeaders['content-type'] = headers['content-type'];
+            }
+            res.writeHead(headers[':status'], forwardHeaders);
+            req.pipe(forwardReq);
             forwardReq.pipe(res);
-        })
+        });
     }
     else {
         res.writeHead(502);
         res.end('There is no control stream attached.');
     }
-    // forward the request to ptthClient
-    // const req = ptthClientSession.request(headers);
-    // stream.pipe(req);
-    // req.on('response', (headers) => {
-    //     stream.respond(headers);
-    //     req.pipe(stream);
-    // });
 });
 server.on('unknownProtocol', (socket) => {
     if ( control ) {
@@ -58,43 +61,6 @@ server.on('unknownProtocol', (socket) => {
         control = new ClientHttp2Session({}, socket);        
     }
 });
-// server.on('unknownProtocol', ptthListener);
-// server.on('stream', badGatewayRejecter);
-
-
-// const listenFor2PtthConnections = (tlsIncomingConnection) => {
-//     if (tlsIncomingConnection.alpnProtocol === '2ptth') {
-//         const ptthClientSession = new ClientHttp2Session({}, tlsIncomingConnection);
-
-//         // remove this listener (do not allow any more 2ptth connections)
-//         server.removeListener('secureConnection', listenFor2PtthConnection);
-
-//         // bind a working http2 listener
-//         const newHandler = (tlsIncomingConnection) => listenForHttp2Connections(ptthClientSession, tlsIncomingConnection);
-//         server.on('secureConnection', newHandler);
-//     }
-// };
-// server.on('secureConnection', listenForControlConnection);
-//     debug('server connection received');
-//     const clientSession = new ClientHttp2Session({}, tlsIncomingConnection);
-
-//     setTimeout(() => {
-//         debug('calling clientSession.request()');
-//         const req = clientSession.request({ ':authority': 'localhost', ':path': '/foo', ':scheme': 'https' }, { endStream: true });
-//         req.on('response', (headers) => {
-//             debug('status', headers[':status']);
-//             debug('date', headers['date']);
-//         });
-
-//         let data = '';
-//         req.setEncoding('utf8');
-//         req.on('data', (d) => data += d);
-//         req.on('end', () => {
-//             debug(`request ended after sending "${data}"`);
-//             clientSession.destroy();
-//         });
-//     }, 20000);
-// });
 server.listen(8080);
 
 //// CLIENT
@@ -103,8 +69,15 @@ const tlsOutgoingConnection = tls.connect({ 'host': 'localhost', 'port': 8080 })
 tlsOutgoingConnection.on('secureConnect', () => {
     const serverSession = new ServerHttp2Session({}, tlsOutgoingConnection);
     serverSession.on('stream', (stream, headers) => {
-        debug('handled request', headers);
-        stream.respond({ ':status': 200, });
-        stream.end('Transmission is possible!');
+        if ( headers[':method'] === 'POST' ) {
+            debug('a post. but is there an echo in here?');
+            stream.respond({ ':status': 200 });
+            stream.pipe(stream);
+        }
+        else {
+            debug('handled request', headers);
+            stream.respond({ ':status': 200, });
+            stream.end('Transmission is possible!');
+        }
     });
 });
